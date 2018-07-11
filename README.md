ed077d3cd1b5e124145772f360cbcd4fca0f0236
Olivia Lemmelin <olivialemmelin@gmail.com>
2018-07-11 13:44:04 -0700

    Adding project to Github

---
+++ README.md
@@ -0,0 +1,5 @@
+# React Redux HTTP Request
+
+This project is using React and Redux to be able to search for a Github repo name. It will then list the repos with the name inputted or repos with similar names. 
+
+Run `npm install` then `npm start` 
---
+++ src/components/Search.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Search = ({ repos, onSearchTextChanged, onSearch, searchText }) => {
+
+  const repoLinks = 
+    <ul>
+      {repos.map(repo => <li><a href={ repo.url } key={ repo.id }>{ repo.name }</a></li>)}
+    </ul>
+    
+
+  return (
+    <div>
+      <p>Enter a repo name to find all repos: </p>
+      <hr />
+      <input onChange={event => onSearchTextChanged(event.target.value)}/>
+      <button onClick={() => onSearch(searchText)}>Search Github</button>
+
+      <div>{repoLinks}</div>
+     </div>
+  )
+}
+
+
+
+export default Search;
+
+
